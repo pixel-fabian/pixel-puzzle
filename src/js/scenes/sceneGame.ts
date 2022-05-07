5e8887b4e7f274119295324094c1fb04080d1be2
pixel-fabian <82926714+pixel-fabian@users.noreply.github.com>
2022-05-07 23:19:00 +0200

    chore: :construction: WIP start column numbers

--- src/js/scenes/sceneGame.ts
+++ src/js/scenes/sceneGame.ts
@@ -26,7 +26,8 @@ export default class SceneGame extends Phaser.Scene {
     const camera = this.cameras.add(0, 0, 800, 600);
     camera.setBackgroundColor('rgba(50, 50, 50, 1)');
     this._createFields();
-    this._createNumbers();
+    this._createColumnNumbers();
+    this._createRowNumbers();
     this.input.mouse.disableContextMenu();
   }
 
@@ -53,7 +54,7 @@ export default class SceneGame extends Phaser.Scene {
     }
   }
 
-  _createNumbers() {
+  _createRowNumbers() {
     let x = 40;
     let y = 100;
     for (let row = 0; row < this.fields.length; row++) {
@@ -84,4 +85,39 @@ export default class SceneGame extends Phaser.Scene {
       y += this.fieldSize;
     }
   }
+
+  _createColumnNumbers() {
+    let x = 100;
+    let y = 40;
+    console.log(this.fields);
+
+    for (let column = 0; column < this.fields[0].length; column++) {
+      let numberRow = 0;
+      for (let row = 0; row < this.fields.length; row++) {
+        if (this.fields[row][column].getFilled()) {
+          // if filled: Add to number
+          numberRow++;
+        }
+        if (
+          numberRow > 0 &&
+          (!this.fields[row][column].getFilled() ||
+            !this.fields[row][column + 1])
+        ) {
+          // if empty or last one in row: Display number
+          this.add
+            .text(x, y, `${numberRow}`, {
+              fontFamily: 'Nunito',
+              color: '#fff',
+              fontSize: '16px',
+            })
+            .setOrigin(0.5);
+          y += 10;
+          numberRow = 0;
+        }
+      }
+      y = 40;
+      x += this.fieldSize;
+    }
+  }
 }
+
